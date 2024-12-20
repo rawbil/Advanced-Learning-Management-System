7bc38b4e7866108e0b9e397177447b4e7025eb6f
Bil <bildadsimiyu6@gmail.com>
2024-12-20 13:04:33 +0300

    add body parser with payload limit

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,9 @@
 import express from 'express';
 export const app = express();
+import cors from "cors";
+import cookieParser from 'cookie-parser';
+//END OF IMPORTS
+
+//body parser
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({extended: true}));
