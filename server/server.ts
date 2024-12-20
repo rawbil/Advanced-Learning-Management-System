eb3f33a884d630abc4b594d43bef9bc12fa4f3f3
Bil <bildadsimiyu6@gmail.com>
2024-12-20 11:53:01 +0300

    restart server directory

--- server/server.ts
+++
@@ -1,8 +0,0 @@
-import {app} from "./app.ts";
-require("dotenv").config();
-const PORT = process.env.PORT;
-
-
-app.listen(PORT, () => {
-    console.log(`Server is listening on port: ${PORT}`);
-})
