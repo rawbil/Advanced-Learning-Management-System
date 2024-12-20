b5f09d443fdac64345e0f52d595660ae45b0fd3f
Bil <bildadsimiyu6@gmail.com>
2024-12-20 15:22:49 +0300

    cors and error handling

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,40 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from "express";
 export const app = express();
 import cors from "cors";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
+require("dotenv").config();
 //END OF IMPORTS
 
 //body parser
-app.use(express.json({limit: "50mb"}));
-app.use(express.urlencoded({extended: true}));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+//cookie-parser
+app.use(cookieParser());
+
+//cors - Cross-Origin Resource Sharing
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+//testing api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({ success: true, message: "API is working" });
+});
+
+
+
+
+//unknown route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+    const err = new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statusCode = 404;
+    next(err);
+})
+
+//error page
+app.use((req: Request, res: Response, err:any) => {
+    res.status(err.statusCode || 500).json({success: "false", message: err.message || "Internal Server error"})
+})
