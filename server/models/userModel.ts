ff4f2096d094eb9946acc55c0712339b4c345939
Bil <bildadsimiyu6@gmail.com>
2024-12-21 17:42:01 +0300

    update user model

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,2 +1,74 @@
-import mongoose from "mongoose";
-import bcrypt from 'bcrypt';
+import mongoose, { Document, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+//**END OF IMPORTS**
+
+const emailPattern: RegExp =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+  role: string;
+  isVerified: boolean;
+  courses: Array<{ courseId: string }>;
+  comparePassword: (password: string) => Promise<boolean>;
+}
+
+const userSchema: Schema<IUser> = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    validate: {
+      validator: function (value: string) {
+        return emailPattern.test(value);
+      },
+      message: "Please enter a valid email",
+    },
+    unique: true,
+  },
+  password: {
+    String,
+    required: true,
+    minlength: [6, "Password must be at least 6 characters"],
+    select: false,
+  },
+  avatar: {
+    public_id: String,
+    url: String,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  courses: [
+    {
+      courseId: String,
+    },
+  ],
+}, {timestamps: true});
+
+//Hash Password
+userSchema.pre<IUser>('save', async function (next) {
+    if(!this.isModified('password')) {
+        next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+ 
+
+//compare password
