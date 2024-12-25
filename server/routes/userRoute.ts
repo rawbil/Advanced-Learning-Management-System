117dbf90a099ff836c4d7db9526ae2713399ac9c
Bil <bildadsimiyu6@gmail.com>
2024-12-25 18:22:01 +0300

    activate user functionality

--- server/routes/userRoute.ts
+++ server/routes/userRoute.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 const router = express.Router();
-import { registerUser } from '../controllers/userController';
+import { activateUser, registerUser } from '../controllers/userController';
 
 
 //register user
+//api/v1/registration
 router.post('/registration', registerUser);
+
+//activate user
+//api/v1/activate-user
+router.post('/activate-user', activateUser);
 
 export default router;
