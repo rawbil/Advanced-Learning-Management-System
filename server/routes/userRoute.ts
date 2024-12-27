3e9657ef7bbb4bed74d6ce9c5e10c870a8a19119
Bil <bildadsimiyu6@gmail.com>
2024-12-27 17:50:03 +0300

    set login route

--- server/routes/userRoute.ts
+++ server/routes/userRoute.ts
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { activateUser, registerUser } from "../controllers/userController";
+import { activateUser, LoginUser, registerUser } from "../controllers/userController";
 
 //register user
 //api/v1/registration
@@ -9,5 +9,9 @@ router.post("/registration", registerUser);
 //activate user
 //api/v1/activate-user
 router.post("/activate-user", activateUser);
+
+//login user
+//api/v1/login
+router.post("/login", LoginUser);
 
 export default router;
