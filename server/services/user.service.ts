e93a148c33b4c622cf6e271ddd9d97e80f41156c
Bil <bildadsimiyu6@gmail.com>
2025-01-12 11:22:28 +0300

    fetch user from redis rather than database

--- server/services/user.service.ts
+++ server/services/user.service.ts
@@ -1,8 +1,11 @@
 import { Response } from "express";
 import userModel from "../models/userModel";
+import { redis } from "../utils/redis";
 
 //get user by id
 export const getUserById = async (id: string, res: Response) => {
-  const user = await userModel.findById(id);
+  //const user = await userModel.findById(id);
+  const JsonUser = await redis.get(id) as string;
+  const user = JSON.parse(JsonUser);
   res.status(201).json({ success: true, user });
 };
