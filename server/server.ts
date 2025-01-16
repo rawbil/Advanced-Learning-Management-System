59a9fcb4d1fb8769cefcb554937f8111d8ec49df
Bil <bildadsimiyu6@gmail.com>
2025-01-16 11:15:11 +0300

    u

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,15 @@ import {app} from './app';
 import connectDB from './utils/db';
 require("dotenv").config();
 const PORT = process.env.PORT;
+import cloudinary from 'cloudinary'
 
+//cloudinary config
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+//cloudinary supports various image formats including base-64
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
