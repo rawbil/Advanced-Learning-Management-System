8a31aa29e3925a3cdd55877c8f0a6abcb4b8649a
Bil <bildadsimiyu6@gmail.com>
2025-01-30 15:05:42 +0300

    update notification status -- for admin

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -14,3 +14,25 @@ export const getNotifications = catchAsyncErrors(async(req: Request, res: Respon
         return next(new ErrorHandler(error.message, 500));
     }
 })
+
+//update notification status -- only admin
+export const updateNotification = catchAsyncErrors(async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {notificationId} = req.body;
+        const notification = await notificationModel.findById(notificationId);
+        if(!notification) {
+            return next(new ErrorHandler("Notification not found", 404));
+        }
+
+        notification.status = "read";
+        await notification.save();
+        
+        //re-arrange notifications
+        const notifications = await notificationModel.find().sort({updatedAt: 1})
+
+        res.status(200).json({success: true, notifications})
+        
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+    }
+})
--- server/routes/notifications.route.ts
+++ server/routes/notifications.route.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { authMiddleware, authorizeRoles } from '../middleware/authMiddleware';
-import { getNotifications } from '../controllers/notification.controller';
+import { getNotifications, updateNotification } from '../controllers/notification.controller';
 const route = express.Router();
 
 //api/v1/get-notifications
 route.post('/get-notifications', authMiddleware, authorizeRoles("admin"), getNotifications);
+//api/v1/update-notification
+route.put('/update-notification', authMiddleware, authorizeRoles("admin"), updateNotification);
 
 export default route;
