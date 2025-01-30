26ed8f7efc7ba70b808a4878554d64ae7bb2dc0b
Bil <bildadsimiyu6@gmail.com>
2025-01-30 21:59:33 +0300

    format code

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -4,35 +4,43 @@ import ErrorHandler from "../utils/ErrorHandler";
 import notificationModel from "../models/notificationModel";
 
 //get all notifications -- only for admin
-export const getNotifications = catchAsyncErrors(async(req: Request, res: Response, next: NextFunction) => {
+export const getNotifications = catchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const notifications = await notificationModel.find().sort({createdAt: -1}) //sort it from latest
+      const notifications = await notificationModel
+        .find()
+        .sort({ createdAt: -1 }); //sort it from latest
 
-        res.status(200).json({success: true, notifications })
-        
+      res.status(200).json({ success: true, notifications });
     } catch (error: any) {
-        return next(new ErrorHandler(error.message, 500));
+      return next(new ErrorHandler(error.message, 500));
     }
-})
+  }
+);
 
 //update notification status -- only admin
-export const updateNotification = catchAsyncErrors(async(req: Request, res: Response, next: NextFunction) => {
+export const updateNotification = catchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const {notificationId} = req.body;
-        const notification = await notificationModel.findById(notificationId);
-        if(!notification) {
-            return next(new ErrorHandler("Notification not found", 404));
-        }
+      const { notificationId } = req.body;
+      const notification = await notificationModel.findById(notificationId);
+      if (!notification) {
+        return next(new ErrorHandler("Notification not found", 404));
+      }
 
-        notification.status === "unread" ? notification.status = "read": notification.status;
-        await notification.save();
-        
-        //re-arrange notifications
-        const notifications = await notificationModel.find().sort({updatedAt: 1})
+      notification.status === "unread"
+        ? (notification.status = "read")
+        : notification.status;
+      await notification.save();
 
-        res.status(200).json({success: true, notifications})
-        
+      //re-arrange notifications
+      const notifications = await notificationModel
+        .find()
+        .sort({ updatedAt: 1 });
+
+      res.status(200).json({ success: true, notifications });
     } catch (error: any) {
-        return next(new ErrorHandler(error.message, 500));
+      return next(new ErrorHandler(error.message, 500));
     }
-})
+  }
+);
