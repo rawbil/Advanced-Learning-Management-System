1ec123416d4776e566bd220e6f4bdad4f85f2aea
Bil <bildadsimiyu6@gmail.com>
2025-01-31 13:20:22 +0300

    add get all users service

--- server/services/user.service.ts
+++ server/services/user.service.ts
@@ -1,6 +1,8 @@
-import { Response } from "express";
+import { NextFunction, Response } from "express";
 import userModel from "../models/userModel";
 import { redis } from "../utils/redis";
+import catchAsyncErrors from "../middleware/catchAsyncErrors";
+import ErrorHandler from "../utils/ErrorHandler";
 
 //get user by id
 export const getUserById = async (id: string, res: Response) => {
@@ -9,3 +11,16 @@ export const getUserById = async (id: string, res: Response) => {
   const user = JSON.parse(JsonUser);
   res.status(201).json({ success: true, user });
 };
+
+
+//get all users
+export const getAllUsers = catchAsyncErrors(async(req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users = await userModel.find().select("-password").sort({createdAt: -1});
+
+    res.status(200).json({success: true, users});
+    
+  } catch (error: any) {
+    return next(new ErrorHandler(error.message, 500))
+  }
+})
