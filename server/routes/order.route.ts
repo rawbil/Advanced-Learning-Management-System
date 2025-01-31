c84a2ea36afc8a069fc52dac3fa88fda3a4f8d54
Bil <bildadsimiyu6@gmail.com>
2025-01-31 13:58:36 +0300

    add get all orders route

--- server/routes/order.route.ts
+++ server/routes/order.route.ts
@@ -1,9 +1,12 @@
 import express from 'express';
-import { authMiddleware } from '../middleware/authMiddleware';
-import { createOrder } from '../controllers/orderControllers';
+import { authMiddleware, authorizeRoles } from '../middleware/authMiddleware';
+import { createOrder, getAllOrders } from '../controllers/orderControllers';
 const route = express.Router();
 
 //api/v1/create-order
 route.post('/create-order', authMiddleware, createOrder);
+
+//api/v1/get-all-orders
+route.get('/get-all-orders', authMiddleware, authorizeRoles("admin"), getAllOrders);
 
 export default route;
