e58bc24f464cd028b92671f01cab1776e80f9e92
Bil <bildadsimiyu6@gmail.com>
2025-02-07 10:57:14 +0300

    add route to app

--- server/app.ts
+++ server/app.ts
@@ -7,6 +7,7 @@ import userRouter from "./routes/userRoute";
 import courseRouter from './routes/course.route';
 import orderRouter from './routes/order.route'
 import notificationRouter from './routes/notifications.route';
+import analyticsRouter from './routes/analytics.route';
 
 import errorMiddleware from "./middleware/error";
 //END OF IMPORTS
@@ -26,7 +27,7 @@ app.use(
 );
 
 //routes
-app.use("/api/v1", userRouter, courseRouter, orderRouter, notificationRouter);
+app.use("/api/v1", userRouter, courseRouter, orderRouter, notificationRouter, analyticsRouter);
 
 //testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
--- server/routes/analytics.route.ts
+++ server/routes/analytics.route.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import { authMiddleware, authorizeRoles } from '../middleware/authMiddleware';
 import { getUserAnalytics } from '../controllers/analytics.controller';
+import router from './userRoute';
 const route = express.Router();
 
 //users analytics route
 //api/v1/user-analytics
 route.get('/user-analytics', authMiddleware, authorizeRoles("admin"), getUserAnalytics);
+
+export default router;
