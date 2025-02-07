6728a83a8c0590d6594a1b6f6787df50a83934b2
Bil <bildadsimiyu6@gmail.com>
2025-02-07 11:07:35 +0300

    debug

--- server/routes/analytics.route.ts
+++ server/routes/analytics.route.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 import { authMiddleware, authorizeRoles } from '../middleware/authMiddleware';
 import { getUserAnalytics } from '../controllers/analytics.controller';
-import router from './userRoute';
 const route = express.Router();
 
 //users analytics route
 //api/v1/users-analytics
 route.get('/users-analytics', authMiddleware, authorizeRoles("admin"), getUserAnalytics);
 
-export default router;
+export default route;
