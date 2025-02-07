1a73044a2e54d87e2a0ab72b94292b6a12d5b25e
Bil <bildadsimiyu6@gmail.com>
2025-02-07 11:35:40 +0300

    courses analytics

--- server/controllers/analytics.controller.ts
+++ server/controllers/analytics.controller.ts
@@ -3,6 +3,7 @@ import ErrorHandler from "../utils/ErrorHandler";
 import catchAsyncErrors from "../middleware/catchAsyncErrors";
 import { generateLast12MonthsData } from "../utils/analytics";
 import userModel from "../models/userModel";
+import courseModel from "../models/courseModel";
 
 //get users analytics --- only for admin
 export const getUserAnalytics = catchAsyncErrors(async(req: Request, res: Response, next: NextFunction) => {
@@ -14,4 +15,17 @@ export const getUserAnalytics = catchAsyncErrors(async(req: Request, res: Respon
     } catch (error: any) {
        return next(new ErrorHandler(error.message, 500)); 
     }
+})
+
+
+//courses analytics
+export const getCoursesAnalytics = catchAsyncErrors(async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const courses = await generateLast12MonthsData(courseModel);
+
+        res.status(200).json({success: true, message: "Courses Analytics", courses});
+        
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 500)); 
+     }
 })
--- server/routes/analytics.route.ts
+++ server/routes/analytics.route.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { authMiddleware, authorizeRoles } from '../middleware/authMiddleware';
-import { getUserAnalytics } from '../controllers/analytics.controller';
+import { getCoursesAnalytics, getUserAnalytics } from '../controllers/analytics.controller';
 const route = express.Router();
 
 //users analytics route
 //api/v1/users-analytics
 route.get('/users-analytics', authMiddleware, authorizeRoles("admin"), getUserAnalytics);
+//api/v1/courses-analytics
+route.get('/courses-analytics', authMiddleware, authorizeRoles("admin"), getCoursesAnalytics);
 
 export default route;
