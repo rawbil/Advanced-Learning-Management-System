16c6a8aba9025a9158d4f29fe1bf5a827a8b2387
Bil <bildadsimiyu6@gmail.com>
2025-02-07 10:39:08 +0300

    call function with userModel to get total number of users

---
+++ server/controllers/analytics.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+import ErrorHandler from "../utils/ErrorHandler";
+import catchAsyncErrors from "../middleware/catchAsyncErrors";
+import { generateLast12MonthsData } from "../utils/analytics";
+import userModel from "../models/userModel";
+
+//get users analytics --- only for admin
+const getUserAnalytics = catchAsyncErrors(async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const users = await generateLast12MonthsData(userModel);
+        
+    } catch (error: any) {
+       return next(new ErrorHandler(error.message, 500)); 
+    }
+})
