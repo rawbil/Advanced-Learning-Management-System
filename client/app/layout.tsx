71aec2adc4c6ce8e3273fc49e393ba8296966c1b
Bil <bildadsimiyu6@gmail.com>
2025-02-16 12:20:33 +0300

    import theme provider to layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,19 +1,20 @@
 import { Metadata } from "next";
 import "./globals.css";
-import {Poppins} from 'next/font/google'
-import {Josefin_Sans} from 'next/font/google';
+import { Poppins } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
+import { ThemeProvider } from "next-themes";
 
 const poppins = Poppins({
-  weight: ['400', '500', '600', '700'],
-  subsets: ['latin'],
-  variable: "--font-poppins"
-})
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
 
 const Josefin = Josefin_Sans({
-  weight: ['400', '500', '600', '700'],
-  subsets: ['latin'],
-  variable: "--font-josefin"
-})
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-josefin",
+});
 
 export const metadata: Metadata = {
   title: "Learning Management System",
@@ -29,13 +30,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${poppins.variable} ${Josefin.variable}`}>
-      <head>
-   
-      </head>
-      <body className={`dark:bg-black bg-white `} >
-        
+      <head></head>
+      <body className={`dark:bg-black bg-white `}>
+        <ThemeProvider>
           <main>{children}</main>
-        
+        </ThemeProvider>
       </body>
     </html>
   );
