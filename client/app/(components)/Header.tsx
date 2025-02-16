ff8c2525a0648ee450a1b8feaf9d1955ac4577be
Bil <bildadsimiyu6@gmail.com>
2025-02-16 17:29:38 +0300

    links for screenss 800px and above

--- client/app/(components)/Header.tsx
+++ client/app/(components)/Header.tsx
@@ -30,7 +30,7 @@ export default function Header({ open, setOpen, activeItem }: IHeader) {
       <div
         className={`border-b dark:border-[#fffff1c]  h-[80px] z-[80] fixed top-0 left-0 right-0 transition duration-500 dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black  ${active ? "shadow-xl" : "shadow"}`}
       >
-        <div className="w-[95%] 800px:w-[92%] mx-auto py-2 h-full">
+        <div className="w-[95%] 800px:w-[92%] mx-auto py-2 h-full flex">
             <div className="w-full h-[80px] flex items-center justify-between p-3">
                 <Link href={'/'} className={`text-[25px] font-josefin font-[800] text-black dark:text-white`}>LMS</Link>
             </div>  
