9a9f76f9f7926531a711561b7a96e255cfe455c5
Bil <bildadsimiyu6@gmail.com>
2025-02-16 17:11:39 +0300

    add link for each nav item

--- client/app/(utils)/NavItems.tsx
+++ client/app/(utils)/NavItems.tsx
@@ -1,30 +1,42 @@
+import Link from "next/link";
 
 interface INavItems {
-    activeItem: number,
-    isMobile: boolean,
+  activeItem: number;
+  isMobile: boolean;
 }
 
 export const navItemsData = [
-    {
-        name: "Home",
-        url: "/"
-    },
-    {
-        name: "Courses",
-        url: "/courses"
-    },
-    {
-        name: "Policy",
-        url: "/policy"
-    },
-    {
-        name: "FAQ",
-        url: "/faq"
-    }
-]
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Courses",
+    url: "/courses",
+  },
+  {
+    name: "Policy",
+    url: "/policy",
+  },
+  {
+    name: "FAQ",
+    url: "/faq",
+  },
+];
 
-export default function NavItems({activeItem, isMobile}: INavItems) {
-    return (
-        <div>NavItems</div>
-    )
+export default function NavItems({ activeItem, isMobile }: INavItems) {
+  return (
+    <div>
+      <div className="hidden 800px:flex">
+        {navItemsData &&
+          navItemsData.map((item, index) => (
+            <Link href={item.url} key={index} passHref>
+              <span
+                className={`${activeItem === index ? "dark:text-[#37a39a] text-[crimson]" : "dark:text-white text-black"} text-[18px] px-6 font-poppins font-400`}
+              ></span>
+            </Link>
+          ))}
+      </div>
+    </div>
+  );
 }
