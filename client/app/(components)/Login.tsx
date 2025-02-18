fa7e5e36e488e996ccd4dd8410260787f4960f3e
Bil <bildadsimiyu6@gmail.com>
2025-02-18 16:31:39 +0300

    validate form schema with yup and formik --- typeescript

--- client/app/(components)/Login.tsx
+++ client/app/(components)/Login.tsx
@@ -1,7 +1,35 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { FaEye, FaEyeSlash, FaGithub, FaGoogle } from "react-icons/fa";
+import { Dispatch, SetStateAction, useState } from "react";
+
+interface Props {
+  setRoute: Dispatch<SetStateAction<string>>;
+}
+
+//yup schema validation
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid email!")
+    .required("Please enter your email"),
+  password: Yup.string().required("Please enter your password").min(8),
+});
+
+export default function Login({ setRoute }: Props) {
+  const [show, setShow] = useState(false);
+
+  const formik = useFormik({
+    initialValues: { email: "", password: "" },
+    validationSchema: schema,
+    onSubmit: async ({ email, password }) => {
+      console.log(email, password);
+    },
+  });
+
+  const { values, errors, touched, handleChange, handleSubmit } = formik;
+
+  
 
 
-export default function Login() {
-    return (
-        <div>Login</div>
-    )
+  return <div>Login</div>;
 }
--- client/app/(utils)/NavItems.tsx
+++ client/app/(utils)/NavItems.tsx
@@ -33,30 +33,33 @@ export default function NavItems({ activeItem, isMobile }: INavItems) {
             <Link href={item.url} key={index} passHref>
               <span
                 className={`${activeItem === index ? "dark:text-[#37a39a] text-[crimson]" : "dark:text-white text-black"} text-[18px] px-6 font-poppins font-[400]`}
-              >{item.name}</span>
+              >
+                {item.name}
+              </span>
             </Link>
           ))}
       </div>
 
       {isMobile && (
         <div className="800px:hidden mt-5">
-               <Link
-              href={"/"}
-              className={`text-[25px] font-josefin font-[800] text-black dark:text-white flex justify-center mb-5`}
-            >
-              LMS
-            </Link>
-            <div className="w-full text-center py-6 flex flex-col gap-8 items-start ">
+          <Link
+            href={"/"}
+            className={`text-[25px] font-josefin font-[800] text-black dark:text-white flex justify-center mb-5`}
+          >
+            LMS
+          </Link>
+          <div className="w-full text-center py-6 flex flex-col gap-8 items-start ">
             {navItemsData &&
-          navItemsData.map((item, index) => (
-            <Link href={item.url} key={index} passHref>
-              <span
-                className={`${activeItem === index ? "dark:text-[#37a39a] text-[crimson]" : "dark:text-white text-black"} text-[18px] px-6 font-poppins font-400`}
-              >{item.name}</span>
-            </Link>
-          ))}
-            </div>
-            
+              navItemsData.map((item, index) => (
+                <Link href={item.url} key={index} passHref>
+                  <span
+                    className={`${activeItem === index ? "dark:text-[#37a39a] text-[crimson]" : "dark:text-white text-black"} text-[18px] px-6 font-poppins font-400`}
+                  >
+                    {item.name}
+                  </span>
+                </Link>
+              ))}
+          </div>
         </div>
       )}
     </div>
