ae7ad28a43f539cbbcf942571d7e852736380bf8
Bil <bildadsimiyu6@gmail.com>
2025-02-18 20:38:43 +0300

    login page - formik and yup validation, social auth UI

--- client/app/(components)/Login.tsx
+++ client/app/(components)/Login.tsx
@@ -1,6 +1,7 @@
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { FaEye, FaEyeSlash, FaGithub, FaGoogle } from "react-icons/fa";
+import {FcGoogle} from 'react-icons/fc'
 import { Dispatch, SetStateAction, useState } from "react";
 import { styles } from "../styles";
 
@@ -29,9 +30,8 @@ export default function Login({ setRoute }: Props) {
 
   const { values, errors, touched, handleChange, handleSubmit } = formik;
 
-  return (
-    <div>
-      <div className="w-full">
+  return (<div>
+    <div className="w-full">
         <h1 className={`${styles.title}`}>Login with LMS</h1>
 
         <form onSubmit={handleSubmit}>
@@ -46,7 +46,7 @@ export default function Login({ setRoute }: Props) {
               onChange={handleChange}
               id="email"
               placeholder="johndoe@gmail.com"
-              className={`${styles.input} ${errors.email && touched.email ? "border border-red-500" : ""}`}
+              className={`${styles.input} ${errors.email && touched.email ? " border-red-500" : ""}`}
             />
 
             <p className="text-red-500 text-sm">{errors.email}</p>
@@ -57,19 +57,51 @@ export default function Login({ setRoute }: Props) {
               Password:{" "}
             </label>
             <input
-              type="password"
+              type={`${show ? "text" : "password"}`}
               name="password"
               id="password"
               value={values.password}
               onChange={handleChange}
               placeholder="Enter your password"
-              className={`${errors.password && touched.email && "border border-red-500"}  ${styles.input}`}
+              className={`${errors.password && touched.password && "border border-red-500"}  ${styles.input}`}
             />
 
-            <p className="text-red-500 text-sm">{errors.password}</p>
+            {show ? (
+              <span className={`absolute z-1 top-1/2 right-2 cursor-pointer ${errors.password ? "top-[42%]" : ""}`} onClick={() => setShow(!show)}>
+                <FaEyeSlash />
+              </span>
+            ) : (
+              <span className={`absolute z-1 top-1/2 right-2 cursor-pointer ${errors.password ? "top-[42%]" : ""}`} onClick={() => setShow(!show)}>
+                <FaEye />
+              </span>
+            )}
+
+            
+            <p className="text-red-500 text-sm pb-4 block">{errors.password}</p>
           </div>
+
+          {/* submit button */}
+          <button type="submit" className={`flex flex-row justify-center items-center py-3 px-6 rounded-full cursor-pointer bg-[#2190ff] min-h-[45px] w-full text-base font-poppins font-semibold mt-5`}>Login</button>
+          <br />
+
+          {/* social auth */}
+          <div className="flex flex-row justify-between items-center gap-[5%]">
+            <hr className="dark:bg-white dark:shadow-none shadow-sm shadow-black/50 bg-black w-1/2 " />
+            <h1 className="font-josefin uppercase text-lg">or</h1>
+            <hr className="dark:bg-white dark:shadow-none bg-black shadow-sm shadow-black/50 w-1/2 " />
+          </div>
+          <div className="flex items-center justify-center my-3 gap-[10%]">
+            <span className="cursor-pointer " title="login with google">
+                <FcGoogle size={28} />
+            </span>
+            <span className="cursor-pointer" title="login with github">
+                <FaGithub size={28} />
+            </span>
+          </div>
+
+          <h5 className="text-center pt-4 font-poppins text-[14px]">Do not have an account? <span className="text-[#2190ff] pl-1 cursor-pointer" onClick={() => setRoute("Sign-up")}>Sign up</span></h5>
+          
         </form>
-      </div>
     </div>
-  );
+  </div>);
 }
--- client/app/(utils)/CustomModal.tsx
+++ client/app/(utils)/CustomModal.tsx
@@ -24,7 +24,7 @@ export default function CustomModal({
       aria-describedby="modal-modal-description"
     >
      {/*  <Box className="absolute top-[50%] translate-x-1/2 -translate-y-1/2 w-[450px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none"> */}
-     <Box className="w-[450px] max-w-full mx-auto mt-[200px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
+     <Box className="w-[450px] max-w-full mx-auto mt-[80px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
       <Component setOpen={setOpen} setRoute={setRoute} />
       </Box>
     </Modal>
