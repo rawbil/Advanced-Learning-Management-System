035db43ace0218cff5954a289d5d3088773b41ff
Bil <bildadsimiyu6@gmail.com>
2025-02-23 13:16:54 +0300

    redux: disable devTools and update middleware configuration

--- client/redux/store.ts
+++ client/redux/store.ts
@@ -3,7 +3,9 @@ import {configureStore} from '@reduxjs/toolkit';
 const store = configureStore({
     reducer: {
 
-    }
+    },
+    devTools: false,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat()
 });
 
 export default store;
