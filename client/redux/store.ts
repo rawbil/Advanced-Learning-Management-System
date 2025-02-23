de071fb944f419cc98045363aa96debc2405cec0
Bil <bildadsimiyu6@gmail.com>
2025-02-23 14:11:38 +0300

    redux: add apiSlice for data fetching and update store configuration

--- client/redux/store.ts
+++ client/redux/store.ts
@@ -1,8 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit';
+import { apiSlice } from './features/api/apiSlice'
 
 const store = configureStore({
     reducer: {
-
+        api: apiSlice.reducer
     },
     devTools: false,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat()
