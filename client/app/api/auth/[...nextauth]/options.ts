77e6111c23a23fa4175d962d735980fc5500ce61
Bil <bildadsimiyu6@gmail.com>
2025-03-05 14:15:10 +0300

    refactor: add client id and secret for the auth providers

--- client/app/api/auth/[...nextauth]/options.ts
+++ client/app/api/auth/[...nextauth]/options.ts
@@ -2,15 +2,18 @@ import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from 'next-auth/providers/google'
 import GithubProvider from 'next-auth/providers/github'
 
-export const options = {
+const options = {
     providers: [
         GithubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
         }),
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID as string,
-            clientSecret: process.env.GOOGLE_SECRET as string,
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         })
-    ]
+    ],
+    secret: process.env.AUTH_SECRET
 } as AuthOptions
+
+export default options
